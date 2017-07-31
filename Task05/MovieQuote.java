044b834fc6e2b91b076e9a0d3268fc340a77c69c
jc428209 <Mk1998jrk6>
2017-07-31 14:37:44 +1000

    Finish task 5

---
+++ Task05/MovieQuote.java
@@ -0,0 +1,12 @@
+package Task05;
+
+/**
+ * Created by jc428209 on 31/07/17.
+ */
+public class MovieQuote {
+    public static void main(String[] args) {
+        System.out.println("This is sparta!!!");
+        System.out.println("300");
+        System.out.println("2006");
+    }
+}
---
+++ Task05/TableAndChairs.java
@@ -0,0 +1,22 @@
+package Task05;
+
+/**
+ * Created by jc428209 on 31/07/17.
+ */
+public class TableAndChairs {
+    public static void main(String[] args) {
+        System.out.println("X                            X");
+        System.out.println("X                            X");
+        System.out.println("X                            X");
+        System.out.println("X            cV              X");
+        System.out.println("X        XXXXXXXXXXX         X");
+        System.out.println("X        X         X         X");
+        System.out.println("XXXXXX   X         X    XXXXXX");
+        System.out.println("X    X   X         X    X    X");
+        System.out.println("X    X   X         X    X    X");
+        System.out.println("X    X   X         X    X    X");
+        System.out.println("X    X   X         X    X    X");
+        System.out.println("X    X   X         X    X    X");
+
+    }
+}
